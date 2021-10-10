a118c825f6fb7b3dc203268f04fbbf3108686ed1
Kevin K <ktkinsey37@gmail.com>
2021-10-10 16:45:50 -0600

    turning in

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import EightBall from "./EightBall.js"
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <EightBall/>
+    </div>
+  );
+}
+
+export default App;
